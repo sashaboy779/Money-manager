707add15587183260eda7b4d368322124069d4fc
Oleksandr Boichuk <sashaboychuk779@gmail.com>
2020-08-30 10:47:11 +0300

    Add Sqlite and Sql Server data contexts for development and production purposes

---
+++ DataAccessLayer/Entities/Operation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DataAccessLayer.Entities
+{
+    public class Operation
+    {
+        public int OperationId { get; set; }
+        public DateTime OperationDate { get; set; }
+        public decimal Amount { get; set; }
+        public string Note { get; set; }
+        public decimal CurrentBalance { get; set; }
+
+        public int WalletId { get; set; }
+        public virtual Wallet Wallet { get; set; }
+
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
---
+++ DataAccessLayer/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace DataAccessLayer.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+        public virtual IEnumerable<Wallet> Wallets { get; set; } = new List<Wallet>();
+        public virtual IEnumerable<MainCategory> Categories { get; set; } = new List<MainCategory>();
+    }
+}
