1c8051cea552176d8d1f57b9e7a69851d7dc31cb
Oleksandr Boichuk <sashaboychuk779@gmail.com>
2020-08-30 10:49:23 +0300

    Implement repository and unit of work patterns

---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly DataContext Context;
+        private readonly DbSet<TEntity> DbSet;
+
+        public GenericRepository(DataContext context)
+        {
+            Context = context;
+            DbSet = context.Set<TEntity>();
+        }
+
+        public async Task<TEntity> GetAsync(int id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public async Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await DbSet.SingleOrDefaultAsync(predicate);
+        }
+
+        public void Create(TEntity entity)
+        {
+            DbSet.Add(entity);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            DbSet.Remove(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            DbSet.Attach(entity);
+            Context.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
---
+++ DataAccessLayer/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using DataAccessLayer.Entities;
+using DataAccessLayer.Repositories;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IGenericRepository<User> UserRepository { get; }
+        public IGenericRepository<Wallet> WalletRepository { get; }
+        public IGenericRepository<Operation> OperationRepository { get; }
+        public IGenericRepository<MainCategory> CategoryRepository { get; }
+
+        private readonly DataContext context;
+
+        public UnitOfWork(DataContext context,
+            IGenericRepository<User> userRepository,
+            IGenericRepository<Wallet> walletRepository,
+            IGenericRepository<Operation> operationRepository,
+            IGenericRepository<MainCategory> categoryRepository)
+        {
+            this.context = context;
+
+            UserRepository = userRepository;
+            WalletRepository = walletRepository;
+            OperationRepository = operationRepository;
+            CategoryRepository = categoryRepository;
+        }
+
+        public async Task CommitAsync()
+        {
+            await context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
