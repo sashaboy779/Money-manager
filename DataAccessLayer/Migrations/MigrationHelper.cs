a5d6ee91c3ae456e51bdac1c3e5d24f06c4246e7
Oleksandr Boichuk <sashaboychuk779@gmail.com>
2020-08-30 10:53:16 +0300

    Configure migrations for different databases

---
+++ DataAccessLayer/Migrations/MigrationHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace DataAccessLayer.Migrations
+{
+    internal static class MigrationHelper
+    {
+        private const string DirectoryUpLevel = "..";
+        private const string StartupProjectName = "MoneyManagerApi";
+        private const string AppSettings = "appsettings.json";
+        private const string AppSettingsDevelopment= "appsettings.Development.json";
+        public static string ConnectionStringName => "WebApiDb";
+
+        public static string GetConnectionString() 
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), 
+                    DirectoryUpLevel, 
+                    StartupProjectName))
+                .AddJsonFile(AppSettings, optional: false, reloadOnChange: true)
+                .AddJsonFile(AppSettingsDevelopment, optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            return configuration.GetConnectionString(ConnectionStringName);
+        }
+    }
+}
---
+++ DataAccessLayer/Migrations/SqliteContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DataAccessLayer.Migrations
+{
+    internal class SqliteContextFactory : IDesignTimeDbContextFactory<SqliteDataContext>
+    {
+        public SqliteDataContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<SqliteDataContext>();
+            var connectionString = MigrationHelper.GetConnectionString();
+
+            optionsBuilder
+                .UseLazyLoadingProxies()
+                .UseSqlite(connectionString);
+
+            return new SqliteDataContext(optionsBuilder.Options);
+        }
+    }
+}
