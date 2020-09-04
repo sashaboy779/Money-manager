175d287a152f9c9786fc19a15fceb01c95e5cb13
Oleksandr Boichuk <sashaboychuk779@gmail.com>
2020-09-04 13:52:42 +0300

    Configure serilog logging

---
+++ MoneyManagerApi/Infrastructure/Constants/AppConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace MoneyManagerApi.Infrastructure.Constants
+{
+    public static class AppConfiguration
+    {
+        public static string LogFilePath => "{0}\\Logs\\Log.txt";
+        public static string ConnectionStringName => "WebApiDb";
+        public static string BusinessLogicProject => "BusinessLogicLayer";
+        public static string WebApiProject => "MoneyManagerApi";
+    }
+}
--- MoneyManagerApi/Startup.cs
+++ MoneyManagerApi/Startup.cs
@@ -1,40 +1,50 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using InfrastructureLayer;
+using System.IO;
+using MoneyManagerApi.Infrastructure.Constants;
+using AutoMapper;
+using System.Reflection;
 
 namespace MoneyManagerApi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IWebHostEnvironment environment;
+        private readonly IConfiguration configuration;
+        
+        public Startup(IWebHostEnvironment environment, IConfiguration configuration)
         {
-            Configuration = configuration;
+            this.environment = environment;
+            this.configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = configuration.GetConnectionString(AppConfiguration.ConnectionStringName);
+            services.ConfigureDbContext(environment.IsProduction(), connectionString);
+
+            services.AddCors();
             services.AddControllers();
+            services.AddAutoMapper(
+                Assembly.Load(AppConfiguration.WebApiProject),
+                Assembly.Load(AppConfiguration.BusinessLogicProject));
+
             services.ConfigureDependencyInjection();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            var path = Directory.GetCurrentDirectory();
+            loggerFactory.AddFile(String.Format(AppConfiguration.LogFilePath, path));
+
+            app.ConfigureExceptionHandler(loggerFactory.CreateLogger<ErrorDetails>());
 
             app.UseHttpsRedirection();
 
