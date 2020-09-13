1ee5ef4aadaea476e940d3e3f886162e0e50f90d
Oleksandr Boichuk <sashaboychuk779@gmail.com>
2020-09-13 16:19:41 +0300

    Add pages for categories management

---
+++ MoneyManagerUi/Pages/Category/CreateSubcategoryComponent.razor.cs
@@ -0,0 +1,12 @@
+﻿using MoneyManagerUi.Shared.Classes;
+using Microsoft.AspNetCore.Components;
+using CategoryModel = MoneyManagerUi.Data.Category.Category;
+
+namespace MoneyManagerUi.Pages.Category
+{
+    public class CreateSubcategoryComponent : ModalComponent<CategoryModel>
+    {
+        [Parameter]
+        public CategoryModel ParentCategory { get; set; }
+    }
+}
